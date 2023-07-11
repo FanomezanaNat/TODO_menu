c886edeea8f812acdf27c263c718fe56ffd33a61
Fanomezana <hei.fanomezana.2@gmail.com>
2023-07-11 11:31:22 +0300

    feat: Add UI file

---
+++ UI.java
@@ -0,0 +1,14 @@
+package todo;
+
+public class UI {
+    public static void main(String[] args) {
+        System.out.println("=== TODO Menu ===");
+        System.out.println("1.Add a new todo");
+        System.out.println("2.Find a todo");
+        System.out.println("3.Show todo");
+        System.out.println("4.Update a todo");
+        System.out.println("5.Delete a todo");
+        System.out.println("6.Quit");
+
+    }
+}
