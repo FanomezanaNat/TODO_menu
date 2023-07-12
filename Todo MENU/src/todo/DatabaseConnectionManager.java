029ae26c2c8bfb30c0a996e11ad0375f259b2e4b
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2023-07-12 11:40:13 +0300

    feat: facilitate db connection

--- Todo MENU/src/todo/DatabaseConnectionManager.java
+++ Todo MENU/src/todo/DatabaseConnectionManager.java
@@ -7,9 +7,17 @@ import java.sql.SQLException;
 import static todo.Settings.*;
 
 public class DatabaseConnectionManager {
+    private String url;
+    private String user;
+    private String password;
+
+
     public Connection getConnection() {
+        this.url = "jdbc:postgresql://localhost/" + DATABASE;
+        this.user = USERNAME;
+        this.password = PASSWORD;
         try {
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            return DriverManager.getConnection(this.url, this.user, this.password);
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
--- Todo MENU/src/todo/Settings.java
+++ Todo MENU/src/todo/Settings.java
@@ -1,7 +1,7 @@
 package todo;
 
 public class Settings {
-    public static  String URL = "jdbc:postgresql://localhost/todolist";
+    public static  String DATABASE = "todolist";
     public static  String USERNAME = "nathan";
     public static  String PASSWORD = "fanomezana";
 
